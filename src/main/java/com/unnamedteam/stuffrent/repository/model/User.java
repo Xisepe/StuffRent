6f4740540d7b5dfa8e287bb0dfa58721d8f3b269
Maxim <maxim.golubeff2014@gmail.com>
2022-07-06 13:33:11 +0700

    User class impl User repository interface

--- src/main/java/com/unnamedteam/stuffrent/repository/model/User.java
+++ src/main/java/com/unnamedteam/stuffrent/repository/model/User.java
@@ -1,4 +1,19 @@
 package com.unnamedteam.stuffrent.repository.model;
 
+import com.unnamedteam.stuffrent.model.Contact;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    @Embedded
+    private Contact contact;
 }
