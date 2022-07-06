53d112b273d116621654739684ec9657fbe9855e
Maxim <maxim.golubeff2014@gmail.com>
2022-07-06 23:49:37 +0700

    swagger

---
+++ src/main/java/com/unnamedteam/stuffrent/config/DocsConfig.java
@@ -0,0 +1,26 @@
+//package com.unnamedteam.stuffrent.config;
+//
+//import io.swagger.v3.oas.annotations.ExternalDocumentation;
+//import io.swagger.v3.oas.annotations.info.Info;
+//import io.swagger.v3.oas.annotations.info.License;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import org.springdoc.core.GroupedOpenApi;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DocsConfig {
+//    @Bean
+//    public OpenAPI springShopOpenAPI() {
+//        return new OpenAPI()
+//                .info(new Info().title("SpringShop API")
+//                        .description("Spring shop sample application")
+//                        .version("v0.0.1")
+//                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+//                .externalDocs(new ExternalDocumentation()
+//                        .description("SpringShop Wiki Documentation")
+//                        .url("https://springshop.wiki.github.org/docs"));
+//    }
+//
+//
+//}
