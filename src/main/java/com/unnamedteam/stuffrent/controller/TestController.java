a4283ce20bdd5f14c84356384b77cc1109d42966
Maxim <maxim.golubeff2014@gmail.com>
2022-07-07 14:04:02 +0700

    a little changes

--- src/main/java/com/unnamedteam/stuffrent/controller/TestController.java
+++ src/main/java/com/unnamedteam/stuffrent/controller/TestController.java
@@ -1,17 +1,8 @@
 package com.unnamedteam.stuffrent.controller;
 
-import com.unnamedteam.stuffrent.filters.jwt.JwtProvider;
-import com.unnamedteam.stuffrent.model.Users;
-import com.unnamedteam.stuffrent.service.UserService;
-import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-import static com.unnamedteam.stuffrent.model.SecurityConstants.HEADER_STRING;
 
 
 @RestController
